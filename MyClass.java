efb7cf68daedf4b03b32366f60a1d5aea49da7da
voKry <133140199+voKry@users.noreply.github.com>
2024-02-12 20:56:50 +0300

    Add files via upload

--- MyClass.java
+++ MyClass.java
@@ -1,8 +1,11 @@
 import java.io.IOException;
+import java.util.Collections;
+import java.util.NavigableMap;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class MyClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ArithmeticException {
         Scanner userEnterNum = new Scanner(System.in);
 
         String enteredPhrase = userEnterNum.nextLine();
@@ -15,39 +18,24 @@ public class MyClass {
         };
         try {
             if (isValid(withoutSpace[0]) != isValid(withoutSpace[2])){
-                try {
-                    throw new IOException();
-                } catch (IOException e) {
-                    System.out.println("Разные системы счисления");
-                }
-
+                throw new ArithmeticException("Разные системы счисления");
             } else {
 
                 if (withoutSpace.length > 3) {
-                    try {
-                        throw new IOException();
-                    } catch (IOException e) {
-                        System.out.println("Элементов в выражении больше двух");
-                    }
-
-
+                    throw new ArithmeticException("Элементов в выражении больше двух");
                 } else {
 
                     if (isValid(withoutSpace[0]) && isValid(withoutSpace[2])) {
                         int result = calculateRimNum(converterRimToArab(rimNumbers, withoutSpace[0]),
                                 converterRimToArab(rimNumbers, withoutSpace[2]), withoutSpace[1]);
                         if (result <= 0) {
-                            try {
-                                throw new IOException();
-                            } catch (IOException e) {
-                                System.out.println("Ошибка: В римской системе нет отрицательных чисел");
-                            }
+                            throw new ArithmeticException("В римской системе нет отрицательных чисел");
                         } else {
-                            System.out.println(rimNumbers[result - 1]);
+                            System.out.println(intToRoman(result));
                         }
                     } else {
                         if ((Integer.parseInt(withoutSpace[0]) > 10) || (Integer.parseInt(withoutSpace[2])) > 10) {
-                            System.out.println("Error: First or seconds number more than 10");
+                            throw new ArithmeticException("Одно из введенных чисел больше 10");
                         } else {
                             System.out.println(calculateArabicNum(withoutSpace[0], withoutSpace[2], withoutSpace[1]));
                         }
@@ -56,7 +44,7 @@ public class MyClass {
             }
 
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Не является математической операцией");
+            throw new ArithmeticException("Не является математической операцией");
         }
 
 
@@ -115,7 +103,40 @@ public class MyClass {
 
     }
     public static boolean isValid (String word) {
+
         return word.matches("[I,V,X]+");
+    }
+
+    public static String intToRoman(int number) {
+        if (number >= 4000 || number <= 0)
+            return null;
+        StringBuilder result = new StringBuilder();
+        for(Integer key : units.descendingKeySet()) {
+            while (number >= key) {
+                number -= key;
+                result.append(units.get(key));
+            }
+        }
+        return result.toString();
+    }
+
+    private static final NavigableMap<Integer, String> units;
+    static {
+        NavigableMap<Integer, String> initMap = new TreeMap<>();
+        initMap.put(1000, "M");
+        initMap.put(900, "CM");
+        initMap.put(500, "D");
+        initMap.put(400, "CD");
+        initMap.put(100, "C");
+        initMap.put(90, "XC");
+        initMap.put(50, "L");
+        initMap.put(40, "XL");
+        initMap.put(10, "X");
+        initMap.put(9, "IX");
+        initMap.put(5, "V");
+        initMap.put(4, "IV");
+        initMap.put(1, "I");
+        units = Collections.unmodifiableNavigableMap(initMap);
     }
 
 }
